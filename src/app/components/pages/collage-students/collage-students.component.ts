a6f22439af2daa002b25797d37e16ac26cc4f30d
JDVega6 <juanguativa07@gmail.com>
2024-02-19 18:58:44 -0500

    Add collage-students.component

---
+++ src/app/components/pages/collage-students/collage-students.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collage-students',
+  standalone: true,
+  imports: [],
+  templateUrl: './collage-students.component.html',
+  styleUrl: './collage-students.component.css'
+})
+export class CollageStudentsComponent {
+
+}
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { RelevanteComponent } from "../relevante/relevante.component";
 import { OfertaComponent } from "../oferta/oferta.component";
 import { MainBtnComponent } from "../main-btn/main-btn.component";
 import { TestimonialsComponent } from "../testimonials/testimonials.component";
+import { CollageStudentsComponent } from "../collage-students/collage-students.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [RelevanteComponent, OfertaComponent, MainBtnComponent, TestimonialsComponent]
+    imports: [RelevanteComponent, OfertaComponent, MainBtnComponent, TestimonialsComponent, CollageStudentsComponent]
 })
 export class HomeComponent {
 
