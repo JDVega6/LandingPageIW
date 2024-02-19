0e0da25fb8ef0110fbdcb34c5bb944f54f059665
JDVega6 <juanguativa07@gmail.com>
2024-02-19 05:03:24 -0500

    Fix testimonials.component

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RelevanteComponent } from "../relevante/relevante.component";
 import { OfertaComponent } from "../oferta/oferta.component";
 import { MainBtnComponent } from "../main-btn/main-btn.component";
+import { TestimonialsComponent } from "../testimonials/testimonials.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [RelevanteComponent, OfertaComponent, MainBtnComponent]
+    imports: [RelevanteComponent, OfertaComponent, MainBtnComponent, TestimonialsComponent]
 })
 export class HomeComponent {
 
