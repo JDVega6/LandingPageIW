d25f9864f9379d4dfc4d89f89656683844e6bbbd
JDVega6 <juanguativa07@gmail.com>
2024-02-19 02:57:29 -0500

    Add main-btn.component

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RelevanteComponent } from "../relevante/relevante.component";
 import { OfertaComponent } from "../oferta/oferta.component";
+import { MainBtnComponent } from "../main-btn/main-btn.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [RelevanteComponent, OfertaComponent]
+    imports: [RelevanteComponent, OfertaComponent, MainBtnComponent]
 })
 export class HomeComponent {
 
--- src/app/components/pages/relevante/relevante.component.ts
+++ src/app/components/pages/relevante/relevante.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MainBtnComponent } from "../main-btn/main-btn.component";
 
 @Component({
-  selector: 'app-relevante',
-  standalone: true,
-  imports: [],
-  templateUrl: './relevante.component.html',
-  styleUrl: './relevante.component.css'
+    selector: 'app-relevante',
+    standalone: true,
+    templateUrl: './relevante.component.html',
+    styleUrl: './relevante.component.css',
+    imports: [MainBtnComponent]
 })
 export class RelevanteComponent {
 
